c0ef7ac66b4cd867d5412e335a20d602ae11ac5c
Kersten Burkhardt <kerstenk@gmail.com>
2012-03-24 01:02:23 +0100

    Cleaned some Code

--- public/js/views/RecordingsView.js
+++ public/js/views/RecordingsView.js
@@ -1,42 +1,52 @@
 var RecordingsView = Backbone.View.extend({
     template: "RecordingsTemplate",
-    
+
     events: {
         'click .tabs > li': 'switchSection',
         'shown a[data-toggle="tab"]': 'switchSection'
     },
-    
+
     initialize: function () {
-        $(this.el).html(_.template( $('#' + this.template).html(), {} ));
-        
+        "use strict";
+
+        $(this.el).html(_.template($('#' + this.template).html(), {}));
+
         if (this.options.section === undefined) {
             this.options.section = 'Recordings';
         }
-        
+
         this.loadSection();
     },
 
-    render: function () {
-        return this;
-    },
-    
     switchSection: function (e) {
+        "use strict";
+
         this.options.section = $(e.target).data('view');
 
-        $('#' + $(e.target).data('view')).addClass('active');
         $('#' + $(e.relatedTarget).data('view')).removeClass('active');
-        
+
         this.loadSection();
     },
 
-    loadSection: function (e) {
+    loadSection: function () {
+        "use strict";
+
+        $('a[data-view="' + this.options.section + '"]', this.el).parent().addClass('active');
+        $('#' + this.options.section, this.el).addClass('active');
+
         if (this.subView != null) {
             this.subView.remove();
         }
-        
+
         this.subView = new window['Recordings' + this.options.section + 'View']({});
         $('#' + this.options.section, this.el).html(this.subView.render().el);
-        
+
         Backbone.history.navigate('!/Recordings/' + this.options.section);
+    },
+
+    render: function () {
+        "use strict";
+
+        return this;
     }
 });
